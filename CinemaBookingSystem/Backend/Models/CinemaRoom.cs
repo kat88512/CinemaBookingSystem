7479e2701aad959a4a60eb5dddb1f153325604e5
kat88512 <kasiapost01@gmail.com>
2024-12-05 17:21:48 +0100

    GenerateSeats in CinemaRoom

--- CinemaBookingSystem/Backend/Models/CinemaRoom.cs
+++ CinemaBookingSystem/Backend/Models/CinemaRoom.cs
@@ -6,6 +6,31 @@ namespace Cinema.Backend.Models
     {
         public int Id { get; private set; }
         public CinemaRoomType Type { get; private set; }
+
         public List<Seat> Seats { get; private set; }
+
+        public CinemaRoom(CinemaRoomType type)
+        {
+            Type = type;
+            Seats = GenerateSeats();
+        }
+
+        private List<Seat> GenerateSeats()
+        {
+            var seats = new List<Seat>();
+
+            var rowsCount = Type.RowsCount;
+            var seatsPerRowCount = Type.SeatsPerRowCount;
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                for (int j = 0; j < seatsPerRowCount; j++)
+                {
+                    Seats.Add(new Seat(row: i, number: j));
+                }
+            }
+
+            return seats;
+        }
     }
 }
