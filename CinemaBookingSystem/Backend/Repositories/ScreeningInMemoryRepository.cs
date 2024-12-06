20fdfca758a7891146982169bacc12523814968d
kat88512 <kasiapost01@gmail.com>
2024-12-06 11:15:23 +0100

    Add ScreeningService

---
+++ CinemaBookingSystem/Backend/Repositories/ScreeningInMemoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Cinema.Backend.Models;
+using CinemaBookingSystem.Backend.Repositories.Interfaces;
+
+namespace CinemaBookingSystem.Backend.Repositories
+{
+    internal class ScreeningInMemoryRepository : IScreeningRepository
+    {
+        private readonly ICollection<Screening> _screenings = new List<Screening>();
+
+        public ScreeningInMemoryRepository() { }
+
+        public IEnumerable<Screening> GetAll(Guid cinemaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Screening GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CinemaBookingSystem/Backend/Services/ScreeningService.cs
@@ -0,0 +1,27 @@
+﻿using CinemaBookingSystem.Backend.Repositories.Interfaces;
+
+namespace CinemaBookingSystem.Backend.Services
+{
+    internal class ScreeningService
+    {
+        private readonly IScreeningRepository _screeningRepository;
+
+        public ScreeningService(IScreeningRepository screeningRepository)
+        {
+            _screeningRepository = screeningRepository;
+        }
+
+        public void PrintAll(Guid cinemaId)
+        {
+            var screenings = _screeningRepository.GetAll(cinemaId);
+            var dateFormat = "dd.MM HH:MM";
+
+            foreach (var screening in screenings)
+            {
+                Console.WriteLine(
+                    $"{screening.TimeFrom.ToString(dateFormat)}: {screening.Movie.Name}"
+                );
+            }
+        }
+    }
+}
