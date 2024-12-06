64b1b7d97f54fde08b4b56d26c52077afb2e4353
kat88512 <kasiapost01@gmail.com>
2024-12-06 12:23:27 +0100

    Add seed data for movie names and dates

---
+++ CinemaBookingSystem/Seeders/SeedData.cs
@@ -0,0 +1,20 @@
+﻿namespace CinemaBookingSystem.Seeders
+{
+    internal static class SeedData
+    {
+        public static List<string> MovieNames =>
+            [
+                "The Godfather",
+                "The Shawshank Redemption",
+                "The Dark Knight",
+                "The Big Lebowski",
+                "Gladiator",
+                "Saving Private Ryan",
+                "Amélie",
+                "Seven Samurai",
+                "Friday"
+            ];
+
+        public static DateTime ScreeningDate => DateTime.Now.AddHours(new Random().Next(1, 80));
+    }
+}
