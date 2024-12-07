0e10676400ac546cccf08a2f2e159a07a531bdc7
kat88512 <kasiapost01@gmail.com>
2024-12-07 22:36:39 +0100

    Move writing in color to new ConsoleExtensions

---
+++ CinemaBookingSystem/Extensions/ConsoleExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace CinemaBookingSystem.Extensions
+{
+    internal class ConsoleExtensions
+    {
+        public static void WriteLineInColor(
+            string value,
+            ConsoleColor foregroundColor = Consts.Colors.DefaultForegroundColor,
+            ConsoleColor backgroundColor = Consts.Colors.DefaultBackgroundColor
+        )
+        {
+            SetColors(foregroundColor, backgroundColor);
+            Console.WriteLine(value);
+            ResetToDefaultColors();
+        }
+
+        public static void WriteLineInColor(
+            int value,
+            ConsoleColor foregroundColor = Consts.Colors.DefaultForegroundColor,
+            ConsoleColor backgroundColor = Consts.Colors.DefaultBackgroundColor
+        )
+        {
+            SetColors(foregroundColor, backgroundColor);
+            Console.WriteLine(value);
+            ResetToDefaultColors();
+        }
+
+        public static void WriteInColor(
+            string value,
+            ConsoleColor foregroundColor = Consts.Colors.DefaultForegroundColor,
+            ConsoleColor backgroundColor = Consts.Colors.DefaultBackgroundColor
+        )
+        {
+            SetColors(foregroundColor, backgroundColor);
+            Console.Write(value);
+            ResetToDefaultColors();
+        }
+
+        public static void WriteInColor(
+            int value,
+            ConsoleColor foregroundColor = Consts.Colors.DefaultForegroundColor,
+            ConsoleColor backgroundColor = Consts.Colors.DefaultBackgroundColor
+        )
+        {
+            SetColors(foregroundColor, backgroundColor);
+            Console.Write(value);
+            ResetToDefaultColors();
+        }
+
+        private static void SetColors(ConsoleColor foregroundColor, ConsoleColor backgroundColor)
+        {
+            Console.ForegroundColor = foregroundColor;
+            Console.BackgroundColor = backgroundColor;
+        }
+
+        private static void ResetToDefaultColors()
+        {
+            Console.ForegroundColor = Consts.Colors.DefaultForegroundColor;
+            Console.BackgroundColor = Consts.Colors.DefaultBackgroundColor;
+        }
+    }
+}
