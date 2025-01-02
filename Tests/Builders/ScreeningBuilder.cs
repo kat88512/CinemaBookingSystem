9e6ac5da50206789cc1bbcab80dafa5dde1fbbe6
Kasia Juszczak <147003249+kat88512@users.noreply.github.com>
2025-01-02 22:33:09 +0100

    Add unit tests for adding screening seat to order (#8)
    * Add project for tests
    
    * Add very basic unit tests
    
    * Add screening builder
    
    * Update tests to use Services instead of Requests

---
+++ Tests/Builders/ScreeningBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Models.CinemaModels;
+using Domain.Models.MovieModels;
+using Domain.Models.ScreeningModels;
+
+namespace Tests.Builders
+{
+    public class ScreeningBuilder
+    {
+        private Movie Movie { get; set; }
+        private CinemaRoom CinemaRoom { get; set; }
+        private DateTime TimeFrom { get; set; }
+        private DateTime TimeTo { get; set; }
+        private VideoTechnology VideoTechnology { get; set; }
+
+        private ScreeningBuilder() { }
+
+        public static ScreeningBuilder Create()
+        {
+            return new ScreeningBuilder();
+        }
+
+        public Screening Build()
+        {
+            return new Screening(Movie, CinemaRoom, TimeFrom, TimeTo, VideoTechnology);
+        }
+
+        public ScreeningBuilder WithDefaultData()
+        {
+            var tomorrow = DateTime.Now.AddDays(1);
+
+            Movie = new Movie("Test movie name");
+            CinemaRoom = new CinemaRoom(Guid.NewGuid(), 1, CinemaRoomType.Large);
+            TimeFrom = tomorrow;
+            TimeTo = TimeFrom.AddHours(2);
+            VideoTechnology = VideoTechnology.TwoDimensional;
+
+            return this;
+        }
+
+        public ScreeningBuilder WithTime(DateTime timeFrom, TimeSpan duration)
+        {
+            TimeFrom = timeFrom;
+            TimeTo = timeFrom.Add(duration);
+
+            return this;
+        }
+
+        public ScreeningBuilder WithVideoTechnology(VideoTechnology videoTechnology)
+        {
+            VideoTechnology = videoTechnology;
+
+            return this;
+        }
+    }
+}
---
+++ Tests/OrderTests/OrderServiceTests.cs
@@ -0,0 +1,103 @@
+﻿using DataAccess.Repositories.OrderRepositories;
+using DataAccess.Repositories.ScreeningRepositories;
+using Domain.Models.OrderModels;
+using Domain.Models.ScreeningModels;
+using Services.Services;
+using Tests.Builders;
+
+namespace Tests.OrderTests
+{
+    [TestFixture]
+    internal class OrderServiceTests
+    {
+        private IScreeningRepository _screenings;
+        private IScreeningSeatRepository _screeningSeats;
+        private IOrderRepository _orders;
+
+        private OrderService _orderService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _screenings = new ScreeningInMemoryRepository();
+            _screeningSeats = new ScreeningSeatInMemoryRepository();
+            _orders = new OrderInMemoryRepository();
+
+            _orderService = new OrderService(_orders, _screenings, _screeningSeats);
+        }
+
+        [Test]
+        public void ShouldAddSeatToOrder()
+        {
+            var screening = ScreeningBuilder.Create().WithDefaultData().Build();
+            var screeningSeat = new ScreeningSeat(screening.Id, 5, 5);
+            var order = new Order();
+
+            _screenings.Add(screening);
+            _screeningSeats.Add(screeningSeat);
+            _orders.Add(order);
+
+            _orderService.AddScreeningSeatToOrder(order.Id, screeningSeat.Id);
+
+            var changedOrder = _orders.GetById(order.Id)!;
+            Assert.That(changedOrder.Items.Any(), Is.True);
+        }
+
+        [Test]
+        public void ShouldNotAddSeatWhenScreeningStartTimeIsInThePast()
+        {
+            var yesterday = DateTime.Now.AddDays(-1);
+
+            var screening = ScreeningBuilder
+                .Create()
+                .WithDefaultData()
+                .WithTime(timeFrom: yesterday, duration: TimeSpan.FromHours(2))
+                .Build();
+
+            var screeningSeat = new ScreeningSeat(screening.Id, 5, 5);
+            var order = new Order();
+
+            _screenings.Add(screening);
+            _screeningSeats.Add(screeningSeat);
+            _orders.Add(order);
+
+            _orderService.AddScreeningSeatToOrder(order.Id, screeningSeat.Id);
+
+            var changedOrder = _orders.GetById(order.Id)!;
+            Assert.That(changedOrder.Items.Any(), Is.False);
+        }
+
+        [Test]
+        public void ShouldNotAddSeatWhenSeatIsTaken()
+        {
+            var screening = ScreeningBuilder.Create().WithDefaultData().Build();
+
+            var screeningSeat = new ScreeningSeat(screening.Id, 5, 5, isTaken: true);
+            var order = new Order();
+
+            _screenings.Add(screening);
+            _screeningSeats.Add(screeningSeat);
+            _orders.Add(order);
+
+            _orderService.AddScreeningSeatToOrder(order.Id, screeningSeat.Id);
+
+            var changedOrder = _orders.GetById(order.Id)!;
+            Assert.That(changedOrder.Items.Any(), Is.False);
+        }
+
+        [Test]
+        public void ShouldNotAddSeatWhenScreeningDoesNotExist()
+        {
+            var screeningSeat = new ScreeningSeat(Guid.NewGuid(), 5, 5);
+            var order = new Order();
+
+            _screeningSeats.Add(screeningSeat);
+            _orders.Add(order);
+
+            _orderService.AddScreeningSeatToOrder(order.Id, screeningSeat.Id);
+
+            var changedOrder = _orders.GetById(order.Id)!;
+            Assert.That(changedOrder.Items.Any(), Is.False);
+        }
+    }
+}
