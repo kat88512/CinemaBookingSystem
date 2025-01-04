2d83db24163b692b57e4a52f1f74336747a0bb23
kat88512 <kasiapost01@gmail.com>
2025-01-04 17:36:37 +0100

    Add UserBuilder to tests

---
+++ Tests/Builders/UserBuilder.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Models.UserModels;
+
+namespace Tests.Builders
+{
+    internal class UserBuilder
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        public static UserBuilder Create()
+        {
+            return new UserBuilder();
+        }
+
+        private UserBuilder() { }
+
+        public UserBuilder WithDefaultData()
+        {
+            _firstName = "Adam";
+            _lastName = "Testowy";
+            _email = "test@example.com";
+
+            return this;
+        }
+
+        public User Build()
+        {
+            return new User(_firstName, _lastName, _email);
+        }
+    }
+}
